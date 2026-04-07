05fe6978c9021af75db831c69ca68eeae2a8f6fd
Pritam <pritamp20@Pritams-MacBook-Air.local>
2026-04-07 23:50:00 +0530

    new approach

---
+++ crates/agent/src/models.rs
@@ -0,0 +1,72 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum JobStatus {
+    Pending,
+    Running,
+    Done, 
+    Failed, 
+    Preempted,
+    Stopped
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum NodeStatus {
+    Idle,
+    Busy,
+    Draining,
+    Preempting
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RunJobRequest {
+    pub job_id: String,
+    pub chunk_id: String,
+    pub image: String,
+    pub cpu_limit: f64,
+    pub ram_limit_mb: u64,
+    pub input_path: Option<String>, // local / object path for chunk input data
+    pub command: Option<Vec<String>>,
+    pub env: Option<Vec<String>>
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RunJobResponse {
+    pub accepted: bool,
+    pub message: String,
+    pub container_id: Option<String>,
+    pub status: JobStatus
+}
+
+pub struct StopJobRequest {
+    pub chunk_id: String,
+    pub reason: Option<String>
+}
+
+#[derive(Debug, Clone, Serilalize, Deserialize)]
+pub struct StopJobResponse{
+    pub stopped: bool,
+    pub message: String,
+    pub status: JobStatus
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HeartbestPayload {
+    pub node_id: String,
+    pub cpu_available_pc: f32,
+    pub ram_available_mb: u64,
+    pub disk_available_gb: u64,
+    pub idle_until_epoch_secs: Option<u64>,
+    pub running_chunks: usize,
+    pub is_idle: bool,
+    pub status: NodeStatus
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ChunkStatusUpdate {
+    pub node_id: String,
+    pub job_id: String,
+    pub chunk_id: String,
+    pub status: JobStatus,
+    pub detail: Option<String>
+}
